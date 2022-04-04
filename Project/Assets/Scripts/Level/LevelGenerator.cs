b0590654b30d38219b0268b08b291006fc6bf333
Julien Revel <j.revel@dowino.com>
2022-04-04 17:36:40 +0200

    Update water material + added empty level generator script

---
+++ Project/Assets/Scripts/Level/LevelGenerator.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public Transform tiles;
+    public float tileSpacing = 12;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
